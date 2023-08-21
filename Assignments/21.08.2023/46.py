ee8454ab8edd5f4027e320c2b067f83364ea7b73
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-08-21 22:52:37 +0530

    solved!

---
+++ Assignments/21.08.2023/46.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+
+    def recursion(self, nums: List[int], mask: List[any], ans: List[List[int]],
+                  it: int):
+
+        if (it == len(nums)):
+            ans.append(mask.copy())
+
+        for i in range(0, len(nums)):
+            if mask[i] == "X":
+                mask[i] = nums[it]
+                self.recursion(nums, mask, ans, it + 1)
+                mask[i] = "X"
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        mask = ["X"] * len(nums)
+        self.recursion(nums, mask, ans, 0)
+        return ans
+
+
+s = Solution()
+print(s.permute([1, 5, 8]))
