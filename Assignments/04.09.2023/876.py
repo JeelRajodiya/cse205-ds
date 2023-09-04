d48b0caf7d55558afef4514c869494003be74145
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-09-04 16:58:36 +0530

    876

---
+++ Assignments/04.09.2023/876.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from typing import Optional
+
+
+class ListNode:
+
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        return f'{self.val} -> {self.next}'
+
+
+class Solution:
+
+    def middleNode(self, head):
+        fast = head
+        slow = head
+
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+
+c = ListNode(3, None)
+b = ListNode(2, c)
+a = ListNode(1, b)
+
+s = Solution()
+print(s.middleNode(a))
