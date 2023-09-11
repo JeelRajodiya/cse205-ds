99a48b5c574cac6225bc31ac40a70cf30395a1d0
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-09-11 15:51:24 +0530

    1672

---
+++ Assignments/11.09.2023/1672.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        sums = []
+        for i in accounts:
+            sums.append(sum(i))
+        return max(sums)
